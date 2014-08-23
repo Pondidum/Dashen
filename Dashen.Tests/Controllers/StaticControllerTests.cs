e659a7a33c1860eb2f3d35fd507b464028bf12f8
andy <andy@andydote.co.uk>
2014-08-23 19:33:52 +0100

    tests: add staticController tests

---
+++ Dashen.Tests/Controllers/StaticControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using Dashen.Controllers;
+using Shouldly;
+using Xunit;
+
+namespace Dashen.Tests.Controllers
+{
+	public class StaticControllerTests
+	{
+		private readonly StaticController _controller;
+
+		public StaticControllerTests()
+		{
+			_controller = new StaticController();
+		}
+
+		[Fact]
+		public void When_a_valid_file_is_requested()
+		{
+			var response = _controller.GetDispatch("css/style.css");
+
+			response.StatusCode.ShouldBe(HttpStatusCode.OK);
+		}
+
+		[Fact]
+		public void When_a_non_existing_file_is_requested()
+		{
+			var response = _controller.GetDispatch("wefw/wefwefwefwefwef.css");
+
+			response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+		}
+	}
+}
