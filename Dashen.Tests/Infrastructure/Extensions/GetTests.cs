b0f1f9e1bd2db473325417d855c2dd6e2e6eadb7
andy <andy@andydote.co.uk>
2014-08-24 15:32:28 +0100

    extensions: add tests for get<>

---
+++ Dashen.Tests/Infrastructure/Extensions/GetTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Dashen.Infrastructure;
+using Shouldly;
+using Xunit;
+
+namespace Dashen.Tests.Infrastructure.Extensions
+{
+	public class GetTests
+	{
+
+		[Fact]
+		public void When_the_key_exists()
+		{
+			var dictionary = new Dictionary<string, int>();
+			dictionary["a"] = 1;
+			
+			dictionary
+				.Get("a")
+				.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_the_key_doesnt_exist_and_value_is_a_value_type()
+		{
+			var dictionary = new Dictionary<string, int>();
+			dictionary["a"] = 1;
+
+			dictionary
+				.Get("r")
+				.ShouldBe(0);
+		}
+
+		[Fact]
+		public void When_the_key_doesnt_exist_and_value_is_a_reference_type()
+		{
+			var dictionary = new Dictionary<string, FactAttribute>();
+			dictionary["a"] = new FactAttribute();
+
+			dictionary
+				.Get("r")
+				.ShouldBe(null);
+		}
+	}
+}
---
+++ Dashen/Infrastructure/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Dashen.Infrastructure
+{
+	public static class Extensions
+	{
+		public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+		{
+			return dictionary.Get(key, default(TValue));
+		}
+
+		public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+		{
+			return dictionary.ContainsKey(key) 
+				? dictionary[key] 
+				: defaultValue;
+		}
+	}
+}
