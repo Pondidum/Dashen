bb570d4a3440d47ce1e4ef0500790214f57ee452
andy <andy@andydote.co.uk>
2014-08-25 14:24:53 +0100

    messageHandler: rename to ConsoleLoggingHandler

---
+++ Dashen/Infrastructure/ConsoleLoggingHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dashen.Infrastructure
+{
+	public class ConsoleLoggingHandler : DelegatingHandler
+	{
+		protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+
+			var response = await base.SendAsync(request, cancellationToken);
+
+			sw.Stop();
+
+			Console.WriteLine("{0} : {1}ms : {2}", request.Method, sw.ElapsedMilliseconds, request.RequestUri);
+	
+			return response;
+		}
+	}
+}
