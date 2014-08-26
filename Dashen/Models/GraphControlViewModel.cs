037180cfa09dc8e53987a0a0b5331ea99d6491ca
andy <andy@andydote.co.uk>
2014-08-26 18:36:36 +0100

    controls: add a basic graph control

---
+++ Dashen/Models/GraphControlViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Dashen.Models
+{
+	public class GraphControlViewModel : ControlViewModel
+	{
+		public IEnumerable<KeyValuePair<int, int>> Points { get; set; }
+		public IEnumerable<KeyValuePair<int, string>> XTicks { get; set; }
+	}
+}
