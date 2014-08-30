d5f617606f37a05b1b7b8ce4e40c0442c1ae2034
andy <andy@andydote.co.uk>
2014-08-30 20:00:56 +0100

    graphControlViewModel: initialise collections to empty

--- Dashen/Models/GraphControlViewModel.cs
+++ Dashen/Models/GraphControlViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Dashen.Models
 {
@@ -6,5 +7,13 @@ namespace Dashen.Models
 	{
 		public IEnumerable<KeyValuePair<int, int>> Points { get; set; }
 		public IEnumerable<KeyValuePair<int, string>> XTicks { get; set; }
+		public IEnumerable<KeyValuePair<int, string>> YTicks { get; set; }
+
+		public GraphControlViewModel()
+		{
+			Points = Enumerable.Empty<KeyValuePair<int, int>>();
+			XTicks = Enumerable.Empty<KeyValuePair<int, string>>();
+			YTicks = Enumerable.Empty<KeyValuePair<int, string>>();
+		}
 	}
 }
