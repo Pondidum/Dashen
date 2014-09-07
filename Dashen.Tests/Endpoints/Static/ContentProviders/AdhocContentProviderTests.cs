96005eae977f17c74b3f41cf1212b93024637044
andy <andy@andydote.co.uk>
2014-09-07 21:43:41 +0100

    staticController: add adhocContentProvider

---
+++ Dashen.Tests/Endpoints/Static/ContentProviders/AdhocContentProviderTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Dashen.Endpoints.Static.ContentProviders;
+using Shouldly;
+using Xunit;
+
+namespace Dashen.Tests.Endpoints.Static.ContentProviders
+{
+	public class AdhocContentProviderTests
+	{
+		[Fact]
+		public void When_adding_content_with_existing_fragment()
+		{
+			var adhoc = new AdhocContentProvider();
+
+			adhoc.Add("test", new MemoryStream(), "test/content");
+
+			Should.Throw<Exception>(() => adhoc.Add("test", new MemoryStream(), "test/content"));
+		}
+
+		[Fact]
+		public void When_getting_content_it_can_be_read_multiple_times()
+		{
+			var adhoc = new AdhocContentProvider();
+
+			adhoc.Add("test", StreamFromContent("Test content"), "test/content");
+
+			var content1 = ReadToEnd(adhoc.GetContent("test").Stream);
+			var content2 = ReadToEnd(adhoc.GetContent("test").Stream);
+
+			content1.ShouldBe("Test content");
+			content2.ShouldBe("Test content");
+		}
+
+		private Stream StreamFromContent(string content)
+		{
+			var ms = new MemoryStream();
+			var sw = new StreamWriter(ms);
+
+			sw.Write(content);
+			sw.Flush();
+			ms.Position = 0;
+
+			return ms;
+		}
+
+		private string ReadToEnd(Stream stream)
+		{
+			using (var sr = new StreamReader(stream))
+			{
+				return sr.ReadToEnd();
+			}
+		}
+	}
+}
---
+++ Dashen/Endpoints/Static/ContentProviders/AdhocContentProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Dashen.Infrastructure;
+
+namespace Dashen.Endpoints.Static.ContentProviders
+{
+	public class AdhocContentProvider : IStaticContentProvider
+	{
+		private readonly Dictionary<string, ResourceContent> _resources;
+
+		public AdhocContentProvider()
+		{
+			_resources = new Dictionary<string, ResourceContent>();
+		}
+
+		public void Add(string urlFragment, Stream content, string mimeType)
+		{
+			using (var ms = new MemoryStream())
+			{
+				content.CopyTo(ms);
+
+				_resources.Add(urlFragment, new ResourceContent
+				{
+					StreamBytes = ms.ToArray(),
+					MimeType = mimeType
+				});
+			}
+		}
+
+		public StaticContent GetContent(string urlFragment)
+		{
+			var resource  = _resources.Get(urlFragment);
+
+			if (resource == null)
+			{
+				return null;
+			}
+
+			return new StaticContent
+			{
+				Stream = new MemoryStream(resource.StreamBytes), 
+				MimeType = resource.MimeType
+			};
+		}
+
+		private class ResourceContent
+		{
+			public byte[] StreamBytes { get; set; }
+			public string MimeType { get; set; }
+		}
+	}
+}
