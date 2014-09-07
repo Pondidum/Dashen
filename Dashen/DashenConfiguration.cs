bf7b905a400eca876deba6e4729360da96d02e19
andy <andy@andydote.co.uk>
2014-09-07 22:28:17 +0100

    configuration: expose adhoc resource adding

--- Dashen/DashenConfiguration.cs
+++ Dashen/DashenConfiguration.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using Dashen.Endpoints.Static.ContentProviders;
 
 namespace Dashen
 {
@@ -10,11 +13,29 @@ namespace Dashen
 		public Color HighlightColor { get; set; }
 		public Color LowlightColor { get; set; }
 
+		internal Dictionary<string, AdhocContentProvider.ResourceContent> Resources { get; private set; }
+
 		public DashenConfiguration()
 		{
+			Resources = new Dictionary<string, AdhocContentProvider.ResourceContent>();
+
 			ListenOn = new Uri("http://localhost:8080");
 			HighlightColor = ColorTranslator.FromHtml("#33CC33");
 			LowlightColor = ColorTranslator.FromHtml("#248F24");
+		}
+
+		public void AddResource(string relativeUrlFragment, Stream content, string mimeType)
+		{
+			using (var ms = new MemoryStream())
+			{
+				content.CopyTo(ms);
+				
+				Resources[relativeUrlFragment] = new AdhocContentProvider.ResourceContent
+				{
+					StreamBytes = ms.ToArray(),
+					MimeType = mimeType
+				};
+			}
 		}
 	}
 }
--- Dashen/Initialisation/StaticContentInitialisation.cs
+++ Dashen/Initialisation/StaticContentInitialisation.cs
@@ -1,17 +1,20 @@
 ﻿using System.Drawing;
 using System.Web.Http.SelfHost;
 using Dashen.Endpoints.Static;
+using Dashen.Endpoints.Static.ContentProviders;
 using Dashen.Infrastructure;
 
 namespace Dashen.Initialisation
 {
-	public class ReplacementSourceInitialisation : IDashboardInitialisation
+	public class StaticContentInitialisation : IDashboardInitialisation
 	{
 		private readonly ReplacementSource _replacements;
+		private readonly AdhocContentProvider _adhockContent;
 
-		public ReplacementSourceInitialisation(ReplacementSource replacements)
+		public StaticContentInitialisation(ReplacementSource replacements, AdhocContentProvider adhockContent)
 		{
 			_replacements = replacements;
+			_adhockContent = adhockContent;
 		}
 
 		public void ApplyTo(DashenConfiguration userConfig, HttpSelfHostConfiguration config)
@@ -23,6 +26,10 @@ namespace Dashen.Initialisation
 			_replacements.Add(
 				userConfig.GetMemberName(x => x.LowlightColor),
 				ColorTranslator.ToHtml(userConfig.LowlightColor));
+
+			userConfig
+				.Resources
+				.Each(res => _adhockContent.Add(res.Key, res.Value));
 		}
 	}
 }
