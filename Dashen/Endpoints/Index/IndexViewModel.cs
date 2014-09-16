31c4d2f6be5f6cbaf7bda7730fed7d4847087837
andy <andy@andydote.co.uk>
2014-09-16 18:34:17 +0100

    indexViewModel: make properties non-null on create

--- Dashen/Endpoints/Index/IndexViewModel.cs
+++ Dashen/Endpoints/Index/IndexViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Dashen.Endpoints.Index
 {
 	public class IndexViewModel
 	{
 		public IEnumerable<IndexDisplayViewModel> Definitions { get; set; }
+
+		public IndexViewModel()
+		{
+			Definitions = Enumerable.Empty<IndexDisplayViewModel>();
+		}
 	}
 }
