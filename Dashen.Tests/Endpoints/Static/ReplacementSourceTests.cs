cc6892e657f5eabf6a02a4253ac4a3f640fd7801
andy <andy@andydote.co.uk>
2014-09-17 22:22:50 +0100

    replacementSource: add tests

---
+++ Dashen.Tests/Endpoints/Static/ReplacementSourceTests.cs
@@ -0,0 +1,25 @@
+﻿using Dashen.Endpoints.Static;
+using Shouldly;
+using Xunit;
+
+namespace Dashen.Tests.Endpoints.Static
+{
+	public class ReplacementSourceTests
+	{
+		[Fact]
+		public void When_no_replacements_have_been_added()
+		{
+			var rs = new ReplacementSource();
+
+			rs.Process("this is a !{test}.").ShouldBe("this is a !{test}.");
+		}
+
+		[Fact]
+		public void When_a_replacement_has_been_added()
+		{
+			var rs = new ReplacementSource();
+			rs.Add("test", "win");
+			rs.Process("this is a !{test}.").ShouldBe("this is a win.");
+		}
+	}
+}
