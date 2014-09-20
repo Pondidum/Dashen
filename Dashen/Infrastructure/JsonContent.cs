9fe0a20dd8eb6818726f10b4c9e4e21dc2402ccb
andy <andy@andydote.co.uk>
2014-09-20 22:35:26 +0100

    jsonConvert: fix namespace

--- Dashen/Infrastructure/JsonContent.cs
+++ Dashen/Infrastructure/JsonContent.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Dashen.Endpoints.Stats
+namespace Dashen.Infrastructure
 {
 	public class JsonContent : HttpContent
 	{
