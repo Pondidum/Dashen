c038eb0e0021fcc1d088d65b0ffc3e12c88db099
andy <andy@andydote.co.uk>
2014-11-03 23:42:53 +0000

    routing: map files as extensionless so iis doesnt get in the way

--- Dashen/Endpoints/Static/StaticController.cs
+++ Dashen/Endpoints/Static/StaticController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,15 +11,25 @@ namespace Dashen.Endpoints.Static
 	public class StaticController : ApiController
 	{
 		private readonly List<IStaticContentProvider> _contentProviders;
+		private readonly Dictionary<string, string> _contentRouteMap;
 
 		public StaticController(IStaticContentProvider contentProvider)
 		{
 			_contentProviders = new[] {contentProvider}.ToList();
+			_contentRouteMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+			_contentRouteMap["css/normalize"] = "css/normalize.css";
+			_contentRouteMap["css/foundation"] = "foundation.min.css";
+			_contentRouteMap["css/style"] = "css/style.css";
+			_contentRouteMap["js/jquery"] = "js/jquery-2.1.1.min.js";
+			_contentRouteMap["js/jquery-flot"] = "js/jquery.flot.min.js";
 		}
 
 		public HttpResponseMessage GetDispatch(string url = "")
 		{
-			var content = _contentProviders.Select(cp => cp.GetContent(url)).FirstOrDefault(c => c != null);
+			var path = _contentRouteMap[url];
+
+			var content = _contentProviders.Select(cp => cp.GetContent(path)).FirstOrDefault(c => c != null);
 
 			if (content == null)
 			{
