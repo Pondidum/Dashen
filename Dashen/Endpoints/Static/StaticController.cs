36a325ca1395b3ac7cb3644889c452223763a600
andy <andy@andydote.co.uk>
2014-11-05 23:20:02 +0000

    staticController: fix resource path

--- Dashen/Endpoints/Static/StaticController.cs
+++ Dashen/Endpoints/Static/StaticController.cs
@@ -20,7 +20,7 @@ namespace Dashen.Endpoints.Static
 			_contentRouteMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
 			_contentRouteMap["css/normalize"] = "css/normalize.css";
-			_contentRouteMap["css/foundation"] = "foundation.min.css";
+			_contentRouteMap["css/foundation"] = "css/foundation.min.css";
 			_contentRouteMap["css/style"] = "css/style.css";
 			_contentRouteMap["js/jquery"] = "js/jquery-2.1.1.min.js";
 			_contentRouteMap["js/jquery-flot"] = "js/jquery.flot.min.js";
