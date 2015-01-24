abb7645b83d12d34a2f8d4fa094aae2bcc12ea30
andy <andy@andydote.co.uk>
2015-01-24 15:41:42 +0000

    dashen: rename file to match type

---
+++ Dashen/Controllers/ModelsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace Dashen.Controllers
+{
+	public class ModelsController : ApiController
+	{
+		public HttpResponseMessage Get(string url = "")
+		{
+			return new HttpResponseMessage();
+		}
+	}
+}
