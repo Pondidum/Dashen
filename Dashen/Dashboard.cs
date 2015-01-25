95cc068fc8415d6bffa9216461d7ca7200567a38
andy <andy@andydote.co.uk>
2015-01-25 14:29:06 +0000

    dashboard: register components in modelrepo and view

--- Dashen/Dashboard.cs
+++ Dashen/Dashboard.cs
@@ -6,10 +6,16 @@ namespace Dashen
 	public class Dashboard
 	{
 		private readonly IContainer _container;
+		private readonly ModelRepository _models;
+		private readonly IDGenerator _generator;
+		private readonly View _view;
 
-		public Dashboard(IContainer container)
+		public Dashboard(IContainer container, ModelRepository models, IDGenerator generator, View view)
 		{
 			_container = container;
+			_models = models;
+			_generator = generator;
+			_view = view;
 		}
 
 		public void Add<TComponent, TModel>(Action<TModel> customise)
@@ -20,6 +26,13 @@ namespace Dashen
 			var component = _container.GetInstance<TComponent>();
 			var model = _container.GetInstance<TModel>();
 
+			_models.Register(_generator.NextID(), () =>
+			{
+				customise(model);
+				return model;
+			});
+
+			_view.Register<TComponent, TModel>(component);
 
 		}
 	}
---
+++ Dashen/View.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Dashen
+{
+	public class View
+	{
+		private readonly List<AssetInfo> _assets;
+		private readonly List<string> _jsx;
+
+		public View()
+		{
+			_assets = new List<AssetInfo>();
+			_jsx = new List<string>();
+		}
+
+		public void Register<TComponent, TModel>(TComponent component)
+			where TComponent : Component<TModel>
+			where TModel : Model
+		{
+			_assets.AddRange(component.GetAssets());
+			_jsx.Add(component.GetJsx());
+		}
+
+		public string Render()
+		{
+			var sb = new StringBuilder();
+
+			sb.AppendLine("<html>");
+			sb.AppendLine("<body>");
+
+			sb.AppendLine("<script type=\"text\\jsx\">");
+			_jsx.ForEach(jsx => sb.AppendLine(jsx));
+			sb.AppendLine("</script>");
+
+			sb.AppendLine("</body>");
+			sb.AppendLine("</html>");
+
+			return sb.ToString();
+		}
+
+	}
+}
