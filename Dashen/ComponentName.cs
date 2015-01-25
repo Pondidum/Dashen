cae69f254d4c47d6ded1c04bf818713f4eb184ca
andy <andy@andydote.co.uk>
2015-01-25 17:42:32 +0000

    componentName: should really commit the file

---
+++ Dashen/ComponentName.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Dashen
+{
+	public class ComponentName
+	{
+		private readonly string _name;
+
+		private ComponentName(string name)
+		{
+			_name = name;
+		}
+
+		public ComponentName(object component)
+			:this(component.GetType().Name)
+		{
+		}
+
+		public override int GetHashCode()
+		{
+			return _name.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			var other = obj as ComponentName;
+
+			if (other == null)
+			{
+				return false;
+			}
+
+			return string.Equals(_name, other._name, StringComparison.OrdinalIgnoreCase);
+		}
+
+		public override string ToString()
+		{
+			return _name;
+		}
+
+		public static ComponentName From(string name)
+		{
+			return new ComponentName(name);
+		}
+	}
+}
