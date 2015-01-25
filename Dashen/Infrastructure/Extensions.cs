267df9bc263c3754a56f3622b6181e7fb7eddc1f
andy <andy@andydote.co.uk>
2015-01-25 17:59:25 +0000

    extensions: add ienumerable.foreach

---
+++ Dashen/Infrastructure/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dashen.Infrastructure
+{
+	public static class Extensions
+	{
+		public static void ForEach<T>(this IEnumerable<T> self, Action<T> action)
+		{
+			foreach (var item in self)
+			{
+				action(item);
+			}
+		}
+	}
+}
