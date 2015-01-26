cbcfad3ff6c3a70210bbe6f3f7fca58f39f99006
andy <andy@andydote.co.uk>
2015-01-26 00:02:23 +0000

    content: add content cache, reactjs

---
+++ Dashen/Content/StaticContentCache.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Dashen.Content
+{
+	public class StaticContentCache
+	{
+		private readonly Assembly _assembly;
+
+		public StaticContentCache()
+		{
+			_assembly = typeof(Dashboard).Assembly;
+		}
+
+		public Stream GetContent(string directory, string file)
+		{
+			var path = directory + "." + file;
+			var asmStream = _assembly.GetManifestResourceStream(GetType(), path);
+
+			return asmStream ?? Stream.Null;
+		}
+	}
+}
