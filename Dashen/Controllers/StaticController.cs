d5ae50937c9c0999c3b5465a7c2f1144a7064e0c
andy <andy@andydote.co.uk>
2015-01-26 00:08:55 +0000

    staticController: use static content cache to get file streams. hard code js for now

--- Dashen/Controllers/StaticController.cs
+++ Dashen/Controllers/StaticController.cs
@@ -1,15 +1,29 @@
 ﻿using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using Dashen.Content;
 
 namespace Dashen.Controllers
 {
 	public class StaticController : ApiController
 	{
+		private readonly StaticContentCache _cache;
+
+		public StaticController(StaticContentCache cache)
+		{
+			_cache = cache;
+		}
+
 		public HttpResponseMessage Get(string directory, string file)
 		{
+			var content = _cache.GetContent(directory, file);
+
 			return new HttpResponseMessage
 			{
-				Content = new StringContent(string.Format("Dir: {0}\nFile: {1}", directory, file))
+				Content = new StreamContent(content)
+				{
+					Headers = { ContentType = new MediaTypeHeaderValue("text/javascript") }
+				}
 			};
 		}
 	}
