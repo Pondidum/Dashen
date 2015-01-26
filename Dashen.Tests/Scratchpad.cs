eab85ec6538befebe7c0b6beb9ecba4ca8634df1
andy <andy@andydote.co.uk>
2015-01-26 13:00:10 +0000

    tests: add scratchpad text for running the ui

---
+++ Dashen.Tests/Scratchpad.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using Dashen.Components;
+using Dashen.Content;
+using Newtonsoft.Json;
+using Shouldly;
+using StructureMap;
+using Xunit;
+
+namespace Dashen.Tests
+{
+	public class Scratchpad
+	{
+		[Fact]
+		public void When_running_the_dashboard()
+		{
+
+			var container = new Container(config =>
+			{
+				config.Scan(a =>
+				{
+					a.AssemblyContainingType<Dashboard>();
+					a.WithDefaultConventions();
+				});
+
+				config.For<View>().Singleton();
+				config.For<ModelRepository>().Singleton();
+				config.For<ComponentRepository>().Singleton();
+				config.For<StaticContentCache>().Singleton();
+			});
+
+			var dashboard = container.GetInstance<Dashboard>();
+			dashboard.Add<TextComponent, TextModel>(model => model.Text = "Testing");
+			dashboard.Start().Wait();
+
+			while (true)
+			{
+				Thread.Sleep(1000);
+			}
+		}
+	}
+
+
+
+	
+}
