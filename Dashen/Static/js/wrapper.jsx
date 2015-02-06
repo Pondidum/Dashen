16f5245e6764d6f0fae4d9b3f45cca5529bc0b0f
andy <andy@andydote.co.uk>
2015-02-06 20:44:33 +0000

    wrapper: fix child element creation

--- Dashen/Static/js/wrapper.jsx
+++ Dashen/Static/js/wrapper.jsx
@@ -1,35 +1,36 @@
 var Wrapper = React.createClass({
 
-    loadFromServer: function() {
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            success: function(data) {
-                this.setState({model: data});
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
-    },
+  loadFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({model: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
 
-    getInitialState: function(){
-        return {model: []};
-    },
+  getInitialState: function(){
+    return {model: []};
+  },
 
-    componentDidMount: function() {
-        this.loadFromServer();
-        setInterval(this.loadFromServer, this.props.interval);
-    },
-    render: function () {
-        return (
-            <div className="medium-3 columns">
-                <div className="panel widget">
-                    <h4> {this.state.model.Title}</h4>
-                    <hr />
-                    <this.props.component model={this.state.model} />
-                </div>
-            </div>
-        );
-    }
+  componentDidMount: function() {
+    this.loadFromServer();
+    setInterval(this.loadFromServer, this.props.interval);
+  },
+  render: function () {
+    return (
+      <div className="medium-3 columns">
+        <div className="panel widget">
+          <h4> {this.state.model.Title}</h4>
+          <hr />
+          {React.createElement(window[this.props.component], {model: this.state.model})}
+        </div>
+      </div>
+    );
+  }
 });
+
