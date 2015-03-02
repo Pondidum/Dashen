abd5af8db7bc448108ed9213f1644cb7c2c1430e
andy <andy@andydote.co.uk>
2015-03-02 12:59:04 +0000

    resource: type for resources to use rather than just streams

---
+++ Dashen/Resource.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace Dashen
+{
+	public class Resource
+	{
+		public static Resource Empty = new Resource("", "", new byte[0]);
+
+		public string Name { get; set; }
+		public string MimeType { get; set; }
+		public byte[] Content { get; set; }
+
+		public Resource(string name, string mimeType, Stream contents)
+			: this(name, mimeType, AsBytes(contents))
+		{
+		}
+
+		public Resource(string name, string mimeType, byte[] contents)
+		{
+			Name = name;
+			MimeType = mimeType;
+			Content = contents;
+		}
+
+		private static byte[] AsBytes(Stream stream)
+		{
+			var ms = new MemoryStream();
+			stream.CopyTo(ms);
+
+			return ms.ToArray();
+		}
+	}
+}
