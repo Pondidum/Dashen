b2ce787963595027b8504cdae24719516f4813a0
andy <andy@andydote.co.uk>
2015-03-05 12:19:54 +0000

    static: handle user resources too

--- Dashen/Controllers/StaticController.cs
+++ Dashen/Controllers/StaticController.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web.Http;
@@ -10,15 +11,19 @@ namespace Dashen.Controllers
 	public class StaticController : ApiController
 	{
 		private readonly StaticContentProvider _cache;
+		private readonly DashboardConfiguration _configuration;
 
-		public StaticController(StaticContentProvider cache)
+		public StaticController(StaticContentProvider cache, DashboardConfiguration configuration)
 		{
 			_cache = cache;
+			_configuration = configuration;
 		}
 
 		public HttpResponseMessage Get(string directory, string file)
 		{
-			var resource = _cache.GetContent(directory, file);
+			var resource = directory.EqualsIgnore("user")
+				? GetUserResource(file)
+				: _cache.GetContent(directory, file);
 
 			return new HttpResponseMessage
 			{
@@ -27,6 +32,11 @@ namespace Dashen.Controllers
 					Headers = { ContentType = new MediaTypeHeaderValue(resource.MimeType) }
 				}
 			};
+		}
+
+		private Resource GetUserResource(string filename)
+		{
+			return _configuration.Resources.FirstOrDefault(r => r.Name.EqualsIgnore(filename)) ?? Resource.Empty;
 		}
 	}
 }
